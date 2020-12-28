537bce4ab119c81fc88e0ce7505f3bb26d1ad6ca
Akilah Hunte <ahunt173@gmail.com>
2020-12-28 10:35:07 -0500

    Started javascript coding

--- UFO-Level 1/static/js/app.js
+++ UFO-Level 1/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
+var tbody = d3.select("tbody");
 
+function renderTable(sourceData) {
+    sourceData.forEach(data => {
+        var row = tbody.append("tr");
+        Object.values(data).forEach(value =>{
+    var cell = row.append("td");
+    cell.text(value);
+    });
+    });
+}
+
+
+
+function handlefilter() {
+    d3.event.preventDefult();
+
+    
+}
 // YOUR CODE HERE!
